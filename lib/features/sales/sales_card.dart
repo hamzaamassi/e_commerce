733cdbbee2be9f6def408e42b35546da695a36f9
Ahmad-Shwedeh <78690416+Ahmad-Shwedeh@users.noreply.github.com>
2023-05-25 21:58:36 +0300

    Search about sales

--- lib/features/sales/sales_card.dart
+++ lib/features/sales/sales_card.dart
@@ -12,63 +12,69 @@ class SalesCard extends GetView<SalesController> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      sales.productName,
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Quantity: ${sales.quantity}',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Price: ${sales.totalPrice} \$',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 16),
-                  ],
-                ),
-              ],
+    return Container(
+      margin: EdgeInsets.only(right: 16, left: 16, top: 16),
+      child: Card(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        sales.productName,
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Quantity: ${sales.quantity}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Price: ${sales.totalPrice} \$',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 16),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () => controller.removeProduct(sales.saleId,
-                sales.productId, sales.quantityAfterSale, sales.quantity),
-            style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateColor.resolveWith((states) => Colors.red)),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: ElevatedButton(
+                onPressed: () => controller.removeProduct(sales.saleId,
+                    sales.productId, sales.quantityAfterSale, sales.quantity),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateColor.resolveWith((states) => Colors.red)),
 
-            // controller.showSalesDialog(context, product),
-            child: Text('Delete'),
-          ),
+                // controller.showSalesDialog(context, product),
+                child: Text('Delete'),
+              ),
+            ),
 
-          // product.image.isNotEmpty?
-          //   Image.network(
-          //     product.image,
-          //     height: 100,
-          //     width: 100,
-          //     fit: BoxFit.cover,
-          //   ):
-          //   Image.asset(
-          //     kLogo,
-          //     width: 120,
-          //     height: 120,
-          //   )
-        ],
+            // product.image.isNotEmpty?
+            //   Image.network(
+            //     product.image,
+            //     height: 100,
+            //     width: 100,
+            //     fit: BoxFit.cover,
+            //   ):
+            //   Image.asset(
+            //     kLogo,
+            //     width: 120,
+            //     height: 120,
+            //   )
+          ],
+        ),
       ),
     );
   }
