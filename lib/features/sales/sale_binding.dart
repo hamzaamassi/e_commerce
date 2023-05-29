374943ff36ba280868e26d8d5a61a3884223cbe5
Hamza Amassi <95129785+hamzaamassi@users.noreply.github.com>
2023-05-29 10:20:11 +0300

    finish program app

--- lib/features/sales/sale_binding.dart
+++ lib/features/sales/sale_binding.dart
@@ -4,8 +4,6 @@ import 'package:get/get.dart';
 class SalesBinding extends Bindings {
   @override
   void dependencies() {
-    print("ttt SalesBinding");
-
     Get.lazyPut<SalesController>(() => SalesController());
   }
 }
